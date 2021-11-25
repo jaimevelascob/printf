b2b1e48a72c42d5b523560fd864d8e8ca427baa4
Jaime Velasco Bustamante <jvelasco@student.42madrid.com>
2021-11-25 17:08:22 +0100

    fix error base

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,37 +6,28 @@
 /*   By: jvelasco <jvelasco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 15:14:28 by jvelasco          #+#    #+#             */
-/*   Updated: 2021/11/25 16:52:37 by jvelasco         ###   ########.fr       */
+/*   Updated: 2021/11/25 17:08:04 by jvelasco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_base(unsigned long long nbr, char *base)
+int	ft_putnbr_base(unsigned long long num, char *base)
 {
-	int			i;
-	int			start;
-	long		n[100];
-	int			size;
+	int		i;
+	int		len;
 
+	len = ft_strlen(base);
 	i = 0;
-	size = ft_strlen(base);
-	if (nbr == 0)
+	if (num < 0)
 	{
-		write (1, "0", 1);
-		return (0);
-	}
-		if (nbr < 0) {
 		ft_putchar('-');
-		nbr *= -1;
+		num = -num;
 	}
-	while (nbr)
-	{
-		n[i++] = nbr % size;
-		nbr /= size;
-	}
-	start = i;
-	while (i > 0)
-		ft_putchar(base[n[--i]]);
-	return (start);
+	if (num >= (unsigned long long)len)
+		 i += ft_putnbr_base(num / len, base);
+	ft_putchar(base[(num % len)]);
+	i += 1;
+	return (i);
 }
+
